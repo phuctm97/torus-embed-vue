1957316394376b237d627a8064efcaeff2aa6ef8
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-03-09 15:49:09 +0700

    Integrate with Torus and Web3

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,54 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    <p v-if="!account">
+      You didn't logged in.
+      <button @click="login">Login</button>
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
+    <div class="info-container" v-if="account">
+      <h3>Your account information</h3>
+      <div class="info">
+        <p><strong>Address</strong>: {{ account.address }}</p>
+        <p><strong>Balance</strong>: {{ account.balance }}</p>
+      </div>
+    </div>
+    <!-- <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    </ul> -->
   </div>
 </template>
 
 <script>
+import Torus from "@toruslabs/torus-embed";
+import Web3 from "web3";
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  data: function() {
+    return {
+      account: null,
+    };
+  },
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  methods: {
+    async login() {
+      const torus = new Torus();
+      await torus.init();
+      await torus.login({ enableLogging: false });
+
+      const web3 = new Web3(torus.provider);
+      const address = (await web3.eth.getAccounts())[0];
+      const balance = await web3.eth.getBalance(address);
+      this.account = {
+        address,
+        balance,
+      };
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -54,5 +66,22 @@ li {
 }
 a {
   color: #42b983;
+}
+.info-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.info {
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: flex-start;
+  margin-top: 20px;
+}
+.info p {
+  margin-bottom: 4px;
+  margin-top: 0;
 }
 </style>
